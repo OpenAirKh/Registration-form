e9ce60a44c283fa6d70cfa81f26a17b934a3950c
OpenAirKh <brizgalov.work@gmail.com>
2024-03-18 10:31:22 +0300

    add project

---
+++ main.js
@@ -0,0 +1,67 @@
+const validAge = document.getElementById('age').value;
+const validNumber = document.getElementById('number').value;
+const validEmail = document.getElementById('email').value;
+
+
+// ====NAME====
+
+function validaiterName(name, namePattern) {
+
+ const validName = document.getElementById('name').value;
+
+    if(name.match(namePattern)) {
+
+        return true;
+    } 
+    else {
+        return false;
+    }
+
+};
+function validRegistration() {
+
+    const validName = document.getElementById('name').value;
+    const namePattern = /^[А-ЯЁ][а-яё]+$/;
+    
+    if(validaiterName(validName, namePattern)){
+   
+              alert('Правильно!')
+    
+   } 
+   else {
+             alert('Не правильно!')
+
+   }     
+
+    
+}   ;
+
+// ===SURNAME====
+
+function validSurname(surName, surnamePattern){
+
+    const validSurname = document.getElementById('surname').value;
+
+    if(surName.match(surnamePattern)) {
+
+        return true;
+    } 
+    else {
+        return false;
+    }
+
+};
+
+function validRegistrationSurname(){
+
+    const validSurname = document.getElementById('surname').value;
+    const surnamePattern = /^[А-ЯЁ][а-яё]+$/;
+
+if validSurname (validSurname, surnamePattern){
+
+}
+
+
+
+
+};
